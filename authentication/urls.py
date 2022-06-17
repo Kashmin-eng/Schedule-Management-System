13f3fc0a7562478259cd0638fe36e67f9c298a1e
Kashmin Shrestha <85051713+Kashmin-eng@users.noreply.github.com>
2022-06-17 21:34:09 +0545

    1.1

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,8 @@
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.home, namme="home"),
+    path('', views.home, name="home"),
 ]
--- sms/settings.py
+++ sms/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2748grbl_#kyu04l$z)h$2jrcc#xp9sb@tls!*z1aapay715l#'
+SECRET_KEY = 'django-insecure-y=6&y%x$^x%x_4o(8^%4qb7x-8g105w_$8wax=lix118uc*^7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
